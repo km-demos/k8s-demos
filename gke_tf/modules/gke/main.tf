8b81461d1c3477a2b2587a97a1814b514b8236a3
Kurt Madel <kmadel@mac.com>
2023-08-13 17:53:21 -0400

    fix kubernetes provider

--- gke_tf/modules/gke/main.tf
+++ gke_tf/modules/gke/main.tf
@@ -70,10 +70,10 @@ resource "google_container_node_pool" "primary_nodes" {
 }
 
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.primary.endpoint}"
+  host  = "https://${google_container_cluster.primary.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
   )
 }
 
